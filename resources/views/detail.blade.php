fb9f9084e940174d5a386aeccb1958e0df53377d
Zemasis <vhp28022000@gmail.com>
2025-03-19 18:06:20 +0700

    Buoi2-Session2

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$task->title}}</title>
+</head>
+<body>
+    <h1>{{$task->title}}</h1>
+    <p>{{$task->description}}</p>
+    <p>{{$task->long_description}}</p>
+    <p>{{$task->completed ? 'Yes' : 'No'}}</p>
+    <a href="/">Back</a>
+</body>
+</html>
