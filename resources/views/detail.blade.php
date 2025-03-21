c8b17b8a49298b7e058ec72f320c98cdf32bf3a3
Zemasis <vhp28022000@gmail.com>
2025-03-21 18:45:29 +0700

    Layput

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$task->title}}</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('title', $task->title)
+
+
+@section('content')
     <h1>Number {{$task->id}}</h1>
-    <h3>{{$task->title}}</h3>
     <p>{{$task->description}}</p>
     <p>{{$task->long_description}}</p>
     <p>{{$task->completed ? 'Yes' : 'No'}}</p>
     <a href="/">Back</a>
-</body>
-</html>
+@endsection
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+    @extends('layouts.app')
+    @section('content')
+    @section('title', 'To do app')
     <h1>To do app</h1>
 
     <table>
@@ -39,10 +33,6 @@
                 </tr>
             @endif
         </tr>
-    @isset($name)
-        <h1>Hello world, {{$name}}</h1>
+    </table>
+@endsection
 
-
-    @endisset
-</body>
-</html>
