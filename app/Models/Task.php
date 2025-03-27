242cbf42c084370c2a83e3dd0f76d85eaff9ad40
Zemasis <vhp28022000@gmail.com>
2025-03-27 16:51:02 +0700

    Session-7-refactor

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,5 @@ class Task extends Model
     //
     use HasFactory;
     public $fillable = ['title', 'description', 'long_description', 'completed'];
+
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,14 @@
     @section('content')
     @section('title', 'To do app')
     {{-- <h1>To do app</h1> --}}
-    <a href="{{ route('tasks.create') }}">Create new task</a>
-    <table>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <div class="mb-4">
+        <a class="form-medium text-gray-700 underline decoration-black-500"
+        href="{{ route('tasks.create') }}">Create new task</a>
+
+    </div>
+    <table class="table-auto">
         <tr>
             <th>Task</th>
             <th>Title</th>
@@ -23,6 +29,12 @@
                     <td>{{$task->long_description}}</td>
                     <td>{{$task->completed ? 'Yes' : 'No'}}</td>
                     <td>
+                        <form action="{{ route('tasks.completed', ['id' => $task->id]) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <button type="submit">{{$task->completed!=true?'Complete':'Uncomplete'}}</button>
+                        </form>
+
                         <a href="{{ route('tasks.detail', ['id' => $task->id]) }}">Detail</a>
                         <a href="{{ route('tasks.edit', ['id' => $task->id]) }}">Edit</a>
                         <form action="{{ route('tasks.delete', ['id' => $task->id]) }}" method="POST">
@@ -33,12 +45,19 @@
 
                 </tr>
                 @endforeach
+
             @else
                 <tr>
                     <td colspan="5">No tasks found</td>
                 </tr>
             @endif
+
         </tr>
     </table>
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
 
